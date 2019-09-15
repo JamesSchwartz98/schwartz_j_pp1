5e2af34b927346aec9bb54f5614bc2dba8ec3268
James Schwartz <jwschwartz98@gmail.com>
2019-09-15 18:17:20 -0400

    successfully compiled FileSearch and ScannerPrint with regex output from test.txt to pick out the IP addresses. Switch statements also compiled successfully.

--- FileSearch.java
+++ FileSearch.java
@@ -12,10 +12,15 @@ import java.util.regex.Pattern;
 import java.util.Stack;
 
 public class FileSearch{
-	static Stack<String> lines = new Stack<>();
-
-/**	static void read(){
-		lines forEach(line ->{System.out.println(line);});
-	}/**end read method*/
-
+	public static void parse(String line){
+		/**regex */
+		String pattern = "(((\\d{3})|(\\d{2}))[.]){3}((\\d{3})|(\\d{2}))";
+		/**create a pattern */
+		Pattern p = Pattern.compile(pattern);
+		/**create a match */
+		Matcher m = p.matcher(line);
+		if(m.find()){
+			System.out.println("found value: "+m.group(0));
+		}/**end if */
+	}/**end parse method */
 }/**end class*/
--- ScannerPrint.java
+++ ScannerPrint.java
@@ -11,26 +11,34 @@ import java.util.*;
 import java.io.*;
 
 public class ScannerPrint{
+	static FileSearch fileSearch = new FileSearch();
+
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-
 		System.out.println("Which file in the directory would you like to search?");
 		String filename = scanner.nextLine();
 
 		read(filename);
 
-/**		switch(flag){
+/**		System.out.println("What parameter would you like to search for? 0 - general results, 1 - Search for IPv4 addresses, 2 - Search for usernames");
+		Scanner cases = new Scanner(System.in);
+		int searchCase;
+		searchCase = cases.nextInt();
+*/
+		switch(searchCase){
 			case 0:
 				System.out.println("Printing default results");
-				break
+				System.out.println("There are    unique IP addresses in the log.");
+				System.out.println("There are    unique users in the log.");
+				break;
 			case 1:
 				System.out.println("Printing the IP addresses");
-				break
+				break;
 			case 2:
 				System.out.println("Printing the usernames");
-				break
-*/
-
+				break;
+		}/**end switch statement */
+		System.out.println("End Switch Statement");
 	}/**end main method*/
 
 	public static void read(String filename){
@@ -38,7 +46,7 @@ public class ScannerPrint{
 			File file = new File(filename);
 			Scanner scanner = new Scanner(file);
 			while(scanner.hasNextLine()){
-				System.out.println(scanner.nextLine());
+				fileSearch.parse(scanner.nextLine());
 			}/**end searching while loop */
 			scanner.close();
 		}/**end try */
